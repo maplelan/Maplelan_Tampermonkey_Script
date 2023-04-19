86492f713ed3f61c5f7714c97f6812301a700e67
maplelan <49081669+maplelan@users.noreply.github.com>
2023-04-19 14:05:31 +0800

    Add files via upload

--- 巴哈姆特動畫瘋輔助工具.user.js
+++ 巴哈姆特動畫瘋輔助工具.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         巴哈姆特動畫瘋輔助工具
-// @namespace    https://ani.gamer.com.tw/animeVideo.php
+// @namespace    https://github.com/maplelan/Maplelan_Tampermonkey_Script/blob/main/%E5%B7%B4%E5%93%88%E5%A7%86%E7%89%B9%E5%8B%95%E7%95%AB%E7%98%8B%E8%BC%94%E5%8A%A9%E5%B7%A5%E5%85%B7.user.js
 // @version      1.0.1
 // @description  顯示動畫瘋撥放時間於上架時間列 自動跳過非影片 可開關[自動跳過開頭年齡認證 自動於結尾播放下一集] 可開關[在開始時自動切換至彈幕設定]
 // @author       Maplelan
