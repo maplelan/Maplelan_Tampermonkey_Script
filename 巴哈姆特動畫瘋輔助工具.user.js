182c165578912fcc42e54234e993366720f9b066
maplelan <49081669+maplelan@users.noreply.github.com>
2023-10-29 20:07:20 +0800

    Update 巴哈姆特動畫瘋輔助工具.user.js

--- 巴哈姆特動畫瘋輔助工具.user.js
+++ 巴哈姆特動畫瘋輔助工具.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         巴哈姆特動畫瘋輔助工具
 // @namespace    https://github.com/maplelan/Maplelan_Tampermonkey_Script/blob/main/%E5%B7%B4%E5%93%88%E5%A7%86%E7%89%B9%E5%8B%95%E7%95%AB%E7%98%8B%E8%BC%94%E5%8A%A9%E5%B7%A5%E5%85%B7.user.js
-// @version      1.1.0
-// @description  顯示動畫瘋撥放時間於上架時間列 可開關[自動跳過開頭年齡認證 自動於結尾播放下一集] 可開關[在開始時自動切換至彈幕設定] 幫所有縮圖標題加上滑鼠移上去即顯示
+// @version      1.1.1
+// @description  顯示動畫瘋撥放時間於上架時間列 可開關[自動跳過開頭年齡認證 自動於結尾播放下一集] 可開關[在開始時自動切換至彈幕設定] 將滑鼠移上縮圖標題會以浮動方塊顯示完整標題
 // @author       Maplelan
 // @license MIT
 // @match        *ani.gamer.com.tw/*
